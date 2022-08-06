cafa5acd7fe94b8c11817ef861fcd10940c31cd6
soumyadeep <1422630+sam17@users.noreply.github.com>
2022-08-06 23:09:45 +0530

    haskell improve

--- kata/parens/parens.hs
+++ kata/parens/parens.hs
@@ -1,36 +1,21 @@
-
 import Data.Char
 import System.IO
 
--- wrong but will work
 process_char :: Char -> Int
 process_char x
    | x == '(' = 1 
    | x == ')' = -1
 
-
-check_elem :: Int -> Bool
-check_elem -1 = False
-check_elem x = True
-
 cumulative_sum :: Num a => [a] -> [a]
 cumulative_sum = scanl (+) 0
               
-
 check_parens :: Bool -> String
 check_parens True = "yes"
 check_parens False = "no"
 
-
 process_cumulative :: [Int] -> Bool
 process_cumulative [] = True
-process_cumulative (a:ax) = check_pos(a) && process_cumulative(ax)
-
-check_pos :: Int -> Bool
-check_pos x
-    | x < 0 = False
-    | x >=0 = True
-
+process_cumulative (a:ax) = (a>=0) && process_cumulative(ax)
 
 main = myLoop
 
@@ -39,8 +24,8 @@ myLoop = do done <- isEOF
               then return() 
               else do inp <- getLine
                       let processed = map process_char inp
-                      let result1 = process_cumulative (cumulative_sum processed)
+                      let cumulative_processed = process_cumulative (cumulative_sum processed)
                       let sum_processed = sum processed
-                      let result = check_parens (result1 && sum_processed == 0)
+                      let result = check_parens (cumulative_processed && sum_processed == 0)
                       putStrLn result
                       myLoop
